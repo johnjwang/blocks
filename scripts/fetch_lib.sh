4ab2c81e913e0fac569af53dbc7b719e49e2561d
Jonathan Bendes <jbendes@umich.edu>
2014-10-05 22:58:08 -0400

    Fixed script issue

--- scripts/fetch_lib.sh
+++ scripts/fetch_lib.sh
@@ -8,11 +8,13 @@ fi
 lib=$1
 
 echo "Fetching Debug/$lib"
+mkdir -p $BLOCKS_HOME/lib/$lib/Debug
 scp -P 49552 blocks@bendeshome.com:lib/Debug/$lib.lib $BLOCKS_HOME/lib/$lib/Debug/$lib.lib
 if [ "$?" != "0" ]; then
     echo "Unable to fetch Debug/$lib"
 fi
 echo "Fetching Release/$lib"
+mkdir -p $BLOCKS_HOME/lib/$lib/Release
 scp -P 49552 blocks@bendeshome.com:lib/Release/$lib.lib $BLOCKS_HOME/lib/$lib/Release/$lib.lib
 if [ "$?" != "0" ]; then
     echo "Unable to fetch Release/$lib"
