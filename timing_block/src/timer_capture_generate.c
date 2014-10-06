81c75bb45e443f1c48856e1ad44896151ecc5bbf
Isaac Olson <isaac.olson@skyspecs.com>
2014-10-05 21:17:28 -0400

    intermediate commit, just saving progress

--- timing_block/src/timer_capture_generate.c
+++ timing_block/src/timer_capture_generate.c
@@ -31,26 +31,32 @@ uint32_t cycles_per_pwm_period;
 uint32_t timer_prescaler;
 uint32_t timer_generate_load;
 
-#define TIMEOUT_MSG "cap timed out\r\n"
-#define TIMEOUT_MSG_LEN sizeof(TIMEOUT_MSG)
+static uint32_t timer_bases[NUM_TIMER_BASES] = { TIMER0_BASE,  TIMER1_BASE,  TIMER2_BASE,
+                                                 TIMER3_BASE,  TIMER4_BASE,  TIMER5_BASE,
+                                                WTIMER0_BASE, WTIMER1_BASE, WTIMER2_BASE,
+                                                WTIMER3_BASE, WTIMER4_BASE, WTIMER5_BASE};
 
-#define CAPTURE_LOW_MSG "captured edge low\r\n"
-#define CAPTURE_LOW_MSG_LEN sizeof(CAPTURE_LOW_MSG)
+static uint32_t timer_sys_periphs[NUM_TIMER_BASES] = { SYSCTL_PERIPH_TIMER0,  SYSCTL_PERIPH_TIMER1,
+                                                       SYSCTL_PERIPH_TIMER2,  SYSCTL_PERIPH_TIMER3,
+                                                       SYSCTL_PERIPH_TIMER4,  SYSCTL_PERIPH_TIMER5,
+                                                      SYSCTL_PERIPH_WTIMER0, SYSCTL_PERIPH_WTIMER1,
+                                                      SYSCTL_PERIPH_WTIMER2, SYSCTL_PERIPH_WTIMER3,
+                                                      SYSCTL_PERIPH_WTIMER4, SYSCTL_PERIPH_WTIMER5};
 
-#define CAPTURE_HIGH_MSG "captured edge high\r\n"
-#define CAPTURE_HIGH_MSG_LEN sizeof(CAPTURE_HIGH_MSG)
+static uint32_t timer_selections[NUM_TIMER_SELECTIONS] = {TIMER_A, TIMER_B};
 
-// XXX: put all the pin mux and timer maping into these and group by input / output #
-//		this will let us do for loops for most of the tedious stuff
-//struct timer_capture_t
-//{
-//
-//};
-//
-//struct timer_generate_t
-//{
-//
-//};
+static uint32_t gpio_ports[NUM_GPIO_PORTS] = {GPIO_PORTA_BASE, GPIO_PORTB_BASE, GPIO_PORTC_BASE,
+                                              GPIO_PORTD_BASE, GPIO_PORTE_BASE, GPIO_PORTF_BASE,
+                                              GPIO_PORTG_BASE};
+
+static uint32_t gpio_sys_periphs[NUM_GPIO_PORTS] = {SYSCTL_PERIPH_GPIOA, SYSCTL_PERIPH_GPIOB,
+                                                    SYSCTL_PERIPH_GPIOC, SYSCTL_PERIPH_GPIOD,
+                                                    SYSCTL_PERIPH_GPIOE, SYSCTL_PERIPH_GPIOF,
+                                                    SYSCTL_PERIPH_GPIOG};
+
+
+static timer_cap_gen_t default_timers[NUM_TIMERS] = {0};
+
 static void timer_capture_int_handler(int num);
 static void timer_capture_int_handler0(void);
 static void timer_capture_int_handler1(void);
@@ -71,6 +77,165 @@ static void (*timer_capture_int_handlers[9])(void) = {timer_capture_int_handler0
                                                       timer_capture_int_handler6,
                                                       timer_capture_int_handler7,
                                                       timer_capture_int_handler8};
+
+void timer_default_setup(void)
+{
+    default_timers[TIMER_INPUT_1]  = (timer_cap_gen_t){GPIO_PD3_WT3CCP1, OVERFLOW_60MS,
+                                                       GPIO_PD, GPIO_PIN_3,
+                                                       WTIMER3, TIMERB, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_2]  = (timer_cap_gen_t){GPIO_PD2_WT3CCP0, OVERFLOW_60MS,
+                                                       GPIO_PD, GPIO_PIN_2,
+                                                       WTIMER3, TIMERA, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_3]  = (timer_cap_gen_t){GPIO_PD1_WT2CCP1, OVERFLOW_60MS,
+                                                       GPIO_PD, GPIO_PIN_1,
+                                                       WTIMER2, TIMERB, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_4]  = (timer_cap_gen_t){GPIO_PB6_T0CCP0, OVERFLOW_60MS,
+                                                       GPIO_PB, GPIO_PIN_6,
+                                                       TIMER0, TIMERA, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_5]  = (timer_cap_gen_t){GPIO_PB4_T1CCP0, OVERFLOW_60MS,
+                                                       GPIO_PB, GPIO_PIN_4,
+                                                       TIMER1, TIMERA, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_6]  = (timer_cap_gen_t){GPIO_PB5_T1CCP1, OVERFLOW_60MS,
+                                                       GPIO_PB, GPIO_PIN_5,
+                                                       TIMER1, TIMERB, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_7]  = (timer_cap_gen_t){GPIO_PD6_WT5CCP0, OVERFLOW_60MS,
+                                                       GPIO_PD, GPIO_PIN_6,
+                                                       WTIMER5, TIMERA, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_8]  = (timer_cap_gen_t){GPIO_PB3_T3CCP1, OVERFLOW_60MS,
+                                                       GPIO_PB, GPIO_PIN_3,
+                                                       TIMER3, TIMERB, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_INPUT_9]  = (timer_cap_gen_t){GPIO_PB2_T3CCP0, OVERFLOW_60MS,
+                                                       GPIO_PB, GPIO_PIN_2,
+                                                       TIMER3, TIMERA, CAPGEN_MODE_CAP_RF_EDGE};
+
+    default_timers[TIMER_OUTPUT_1] = (timer_cap_gen_t){GPIO_PG1_T4CCP1, OVERFLOW_20MS,
+                                                       GPIO_PG, GPIO_PIN_4,
+                                                       TIMER4, TIMERB, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_2] = (timer_cap_gen_t){GPIO_PG2_T5CCP0, OVERFLOW_20MS,
+                                                       GPIO_PG, GPIO_PIN_2,
+                                                       TIMER5, TIMERA, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_3] = (timer_cap_gen_t){GPIO_PG3_T5CCP1, OVERFLOW_20MS,
+                                                       GPIO_PG, GPIO_PIN_3,
+                                                       TIMER5, TIMERB, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_4] = (timer_cap_gen_t){GPIO_PC4_WT0CCP0, OVERFLOW_20MS,
+                                                       GPIO_PC, GPIO_PIN_4,
+                                                       WTIMER0, TIMERA, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_5] = (timer_cap_gen_t){GPIO_PC5_WT0CCP1, OVERFLOW_20MS,
+                                                       GPIO_PC, GPIO_PIN_5,
+                                                       WTIMER0, TIMERB, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_6] = (timer_cap_gen_t){GPIO_PC6_WT1CCP0, OVERFLOW_20MS,
+                                                       GPIO_PC, GPIO_PIN_6,
+                                                       WTIMER1, TIMERA, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_7] = (timer_cap_gen_t){GPIO_PC7_WT1CCP1, OVERFLOW_20MS,
+                                                       GPIO_PC, GPIO_PIN_7,
+                                                       WTIMER1, TIMERB, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_8] = (timer_cap_gen_t){GPIO_PB7_T0CCP1, OVERFLOW_20MS,
+                                                       GPIO_PB, GPIO_PIN_7,
+                                                       TIMER0, TIMERB, CAPGEN_MODE_GEN_PWM};
+
+    default_timers[TIMER_OUTPUT_9] = (timer_cap_gen_t){GPIO_PG0_T4CCP0, OVERFLOW_20MS,
+                                                       GPIO_PG, GPIO_PIN_0,
+                                                       TIMER4, TIMERA, CAPGEN_MODE_GEN_PPM};
+}
+
+void timer_default_init(void)
+{
+    timer_default_setup();
+    timer_init(default_timers, NUM_TIMERS);
+}
+
+void timer_init(timer_cap_gen_t timers[], uint8_t num)
+{
+    uint8_t i;
+
+    // XXX can we put an ifdef DEBUG or something here?
+    // Input check
+    for (i=0; i<num; ++i) {
+        if (timers[i].gpio_port_ind >= NUM_GPIO_PORTS) while (1);
+        if (timers[i].timer_base_ind >= NUM_TIMER_BASES) while (1);
+        if (timers[i].timer_sel_ind >= NUM_TIMER_SELECTIONS) while (1);
+        if (    (timers[i].capgen_mode & CAPGEN_MODE_CAP_MASK)
+             && (timers[i].capgen_mode & CAPGEN_MODE_GEN_MASK)) while (1);
+        if (    !(timers[i].capgen_mode & CAPGEN_MODE_CAP_MASK)
+             && !(timers[i].capgen_mode & CAPGEN_MODE_GEN_MASK)) while (1);
+    }
+
+    // Enable system peripherals
+    for (i=0; i<num; ++i) {
+        SysCtlPeripheralEnable(gpio_sys_periphs[timers[i].gpio_port_ind]);
+        SysCtlPeripheralEnable(timer_sys_periphs[timers[i].timer_base_ind]);
+    }
+
+    // Configure GPIOs
+    for (i=0; i<num; ++i) {
+        GPIOPinConfigure(timers[i].gpio_pin_config);
+        GPIOPinTypeTimer(gpio_ports[timers[i].gpio_port_ind], timers[i].gpio_pin);
+    }
+    for (i=0; i<num; ++i) {
+        if (timers[i].capgen_mode & CAPGEN_MODE_CAP_MASK) {
+            GPIOPadConfigSet(gpio_ports[timers[i].gpio_port_ind], timers[i].gpio_pin,
+                             GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPD);
+        }
+    }
+
+    // Configure timers
+    uint32_t timer_cfg[NUM_TIMER_BASES] = {0};
+    for (i=0; i<num; ++i) {
+        switch (timers[i].timer_sel_ind) {
+            case TIMERA:
+                if (timers[i].capgen_mode & CAPGEN_MODE_CAP_MASK) {
+                    timer_cfg[timers[i].timer_base_ind] |= TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_CAP_TIME;
+                } else if (timers[i].capgen_mode & CAPGEN_MODE_GEN_MASK) {
+                    timer_cfg[timers[i].timer_base_ind] |= TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM;
+                }
+                break;
+            case TIMERB:
+                if (timers[i].capgen_mode & CAPGEN_MODE_CAP_MASK) {
+                    timer_cfg[timers[i].timer_base_ind] |= TIMER_CFG_SPLIT_PAIR | TIMER_CFG_B_CAP_TIME;
+                } else if (timers[i].capgen_mode & CAPGEN_MODE_GEN_MASK) {
+                    timer_cfg[timers[i].timer_base_ind] |= TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    for (i=0; i<NUM_TIMER_BASES; ++i) {
+        if (timer_cfg) TimerConfigure(timer_bases[i], timer_cfg[i]);
+    }
+
+    // Calculate load and prescale
+    uint32_t sysclk_cycles_per_second = SysCtlClockGet();
+    uint64_t sysclk_cycles_per_overflow;
+    uint32_t prescaler;
+    uint32_t load;
+    for (i=0; i<num; ++i) {
+         sysclk_cycles_per_overflow = ((uint64_t)timers[i].us_per_overflow *
+                                       (uint64_t)sys_clk_cycles_per_second) / 1000000;
+         if (timers[i].timer_base_ind <= TIMER5) { // narrow timer
+             prescaler = (sysclk_cycles_per_overflow & 0xFFFF0000) >> 16;
+             load      = (sysclk_cycles_per_overflow & 0x0000FFFF);
+         } else { // wide timer
+             prescaler = (sysclk_cycles_per_overflow & 0xFFFFFFFF00000000) >> 16;
+             load      = (sysclk_cycles_per_overflow & 0x00000000FFFFFFFF);
+         }
+    }
+}
 
 void timer_capture_generate_init(void)
 {
@@ -250,7 +415,7 @@ void timer_capture_generate_init(void)
 	TimerIntRegister(WTIMER3_BASE, TIMER_B, timer_capture_int_handlers[7]);
 	TimerIntRegister(WTIMER5_BASE, TIMER_A, timer_capture_int_handlers[8]);
 
-	// Prioritize Capture Interrupt
+	// Prioritize Capture Interrupt, XXX: shouldn't need this, the register call does this
     IntEnable(INT_TIMER0A);
     IntEnable(INT_TIMER1A);
     IntEnable(INT_TIMER1B);
