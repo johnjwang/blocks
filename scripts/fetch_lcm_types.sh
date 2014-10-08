b57144d26565f87b5490c16663d78e5c20334e7c
Jonathan Bendes <jbendes@umich.edu>
2014-10-08 03:22:43 -0400

    Attempt at fixing script

--- scripts/fetch_lcm_types.sh
+++ scripts/fetch_lcm_types.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Fetching embedded lcmtypes"
+mkdir -p $BLOCKS_HOME/c/include/arch/embedded/lcmtypes/
 scp -r -P 49552 blocks@bendeshome.com:src/embedded_lcmtypes/* $BLOCKS_HOME/c/src/arch/embedded/lcmtypes/
 scp -r -P 49552 blocks@bendeshome.com:include/embedded_lcmtypes/* $BLOCKS_HOME/c/include/arch/embedded/lcmtypes/
 if [ "$?" != "0" ]; then
@@ -9,6 +10,7 @@ fi
 echo ""
 
 echo "Fetching linux lcmtypes"
+mkdir -p $BLOCKS_HOME/c/include/arch/linux/lcmtypes/
 scp -r -P 49552 blocks@bendeshome.com:src/linux_lcmtypes/* $BLOCKS_HOME/c/src/arch/linux/lcmtypes/
 scp -r -P 49552 blocks@bendeshome.com:include/linux_lcmtypes/* $BLOCKS_HOME/c/include/arch/linux/lcmtypes/
 if [ "$?" != "0" ]; then
