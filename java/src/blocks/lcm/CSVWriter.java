7813b4a7b34da7ed6db24b93123e867d3bb955e6
Jonathan Bendes <jbendes@umich.edu>
2014-10-10 06:18:54 -0400

    First draft of decoding arbitrary lcmtypes works

--- java/src/blocks/lcm/CSVWriter.java
+++ java/src/blocks/lcm/CSVWriter.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Field;
 import lcm.lcm.*;
 import lcm.spy.*;
 
+import april.util.GetOpt;
 import april.util.TimeUtil;
 
 public class CSVWriter implements LCMSubscriber
@@ -36,20 +37,46 @@ public class CSVWriter implements LCMSubscriber
 
             Object o = cls.getConstructor(DataInput.class).newInstance(dins);
 
-            System.out.println(channel + ": ");
+            System.out.print(channel + ": ");
             Field fields[] = cls.getFields();
             for(Field field : fields)
             {
                 String name = field.getName();
+                if(name.startsWith("LCM_FINGERPRINT"))
+                    continue;
                 Object value = field.get(o);
-                System.out.println(value + "\t");
+                System.out.print(name + ": ");
+                System.out.print(value + "\t");
             }
-            System.out.println("\n");
+            System.out.println("");
         }
         catch (Exception e)
         {
             System.err.println("Encountered error while decoding " + channel + " lcm type");
             e.printStackTrace();
         }
+    }
+
+    public void run()
+    {
+        while(true)
+        {
+            try{Thread.sleep(1000);}catch(Exception e){}
+        }
+    }
+
+    public static void main(String args[])
+    {
+        String lcm_url = null;
+
+        GetOpt gopt = new GetOpt();
+        gopt.addBoolean('h', "help", false, "Show this help");
+
+        if (!gopt.parse(args) || gopt.getBoolean("help")) {
+            gopt.doHelp();
+            return;
+        }
+
+        new CSVWriter(LCM.getSingleton()).run();
     }
 }
